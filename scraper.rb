2e69cff457b2c0743ce3d40cc148412ed07b5fdd
Tony Bowden <tony@tmtm.com>
2015-07-25 11:29:37 +0100

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,78 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+require 'wikidata'
+
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read) 
+end
+
+def wikidata(title)
+  return {} if title.to_s.empty?
+
+  wd = Wikidata::Item.find_by_title title
+
+  property = ->(elem, attr='title') { 
+    prop = wd.property(elem) or return
+    prop.send(attr)
+  }
+
+  fromtime = ->(time) { 
+    return unless time
+    DateTime.parse(time.time).to_date.to_s 
+  }
+
+  # party = P102
+  # freebase = P646
+  return { 
+    wikidata: wd.id,
+    family_name: property.('P734'),
+    given_name: property.('P735'),
+    image: property.('P18', 'url'),
+    gender: property.('P21'),
+    birth_date: fromtime.(property.('P569', 'value')),
+  }
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  section = noko.xpath('.//h3/span[@class="mw-headline" and contains(.,"Electorate Results")]')
+  section.xpath('.//following::table[.//th[contains(.,"Candidate")]]').each do |table|
+    constituency = table.css('tr').first.text[/2010: (.*)/, 1]
+    winner = table.xpath('.//tr[td]').map { |tr|
+      tds = tr.css('td')
+      next if tds.any? { |td| td.attr('colspan') }
+      data = { 
+        party: tds[0].text.tidy,
+        name: tds[1].text.tidy,
+        wikipedia: tds[1].xpath('.//a[not(@class="new")]/@href').text,
+        wikipedia_title: tds[1].xpath('.//a[not(@class="new")]/@title').text,
+        constituency: constituency,
+        votes: tds[2].text.to_i,
+        term: 13,
+        source: url,
+      }
+      data[:wikipedia] = URI.join('https://en.wikipedia.org/', data[:wikipedia]).to_s unless data[:wikipedia].to_s.empty?
+      data
+    }.compact.sort_by { |d| d[:votes] }.reverse.first
+    winner.merge!  wikidata( winner[:wikipedia_title] )
+    puts winner
+    ScraperWiki.save_sqlite([:name, :constituency], winner)
+  end
+end
+
+scrape_list('https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2010')
